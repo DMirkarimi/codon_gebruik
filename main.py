ed651d395ef3023e764f1d70d9df629ec9bcd09e
foziawarsame <71036172+foziawarsame@users.noreply.github.com>
2021-02-23 16:28:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,30 @@ def seperate(nucleotide_dict):
     surface_protein['gene=nef'] = nucleotide_dict['gene=nef']
     return surface_protein, other
 
+aa3 = {"Ala ": ["GCU" , "GCC" , "GCA" , "GCG"] ,
+       "Arg": ["CGU" , "CGC" , "CGA" , "CGG" , "AGA" , "AGG"] ,
+       "Asn": ["AAU" , "AAC"] ,
+       "Asp": ["GAU" , "GAC"] ,
+       "Cys": ["UGU" , "UGC"] ,
+       "Gln": ["CAA" , "CAG"] ,
+       "Glu": ["GAA" , "GAG"] ,
+       "Gly": ["GGU" , "GGC" , "GGA" , "GGG"] ,
+       "His": ["CAU" , "CAC"] ,
+       "Ile": ["AUU" , "AUC" , "AUA"] ,
+       "Leu": ["UUA" , "UUG" , "CUU" , "CUC" , "CUA" , "CUG"] ,
+       "Lys": ["AAA" , "AAG"] ,
+       "Met": ["AUG"] ,
+       "Phe": ["UUU" , "UUC"] ,
+       "Pro": ["CCU" , "CCC" , "CCA" , "CCG"] ,
+       "Ser": ["UCU" , "UCC" , "UCA" , "UCG" , "AGU" ,"AGC"] ,
+       "Thr": ["ACU" , "ACC" , "ACA" , "ACG"] ,
+       "Trp": ["UGG"] ,
+       "Tyr": ["UAU" , "UAC"] ,
+       "Val": ["GUU" , "GUC" , "GUA" , "GUG"] ,
+       "Start": ["AUG" , "CUG" , "UUG" , "GUG" , "AUU"] ,
+       "Stop" : ["UAG" , "UGA" , "UAA"]
+}
+
 
 if __name__ == '__main__':
     x = get_sequences('fasta/virus/siv seq mRNA.fasta')

255dd60903d4ee459d5c2f32e6ede4068d6ef215
Equallax <34425132+Equallax@users.noreply.github.com>
2021-02-23 16:28:03 +0100

    extensions normalized

--- main.py
+++ main.py
@@ -1,1 +1,70 @@
-#"fakka dariush"
+import re
+
+
+def get_sequences(file_path):
+    nucleotide_dict = {}
+    current_seq_list = []
+
+
+
+
+
+    with open(file_path, 'r') as f:
+        current_header = next(f)
+        for raw_line in f:
+            # Removing trailing newlines.
+            line = raw_line.rstrip()
+            # Checking if the current line is a header.
+            if line[0] == '>':
+                # Turning sequence list into one string.
+                current_seq = ''.join(current_seq_list)
+                try:
+                    search = re.search(r'gene=[A-Za-z0-9]*',
+                                       current_header)
+                    gene_name = search.group().lower()
+                except AttributeError:
+                    gene_name = current_header
+
+                nucleotide_dict[gene_name] = current_seq
+
+                # Setting the current line as the new current
+                # header.
+                current_header = line
+                # Resetting the current sequence list because
+                # the previous sequence has ended.
+                current_seq_list = []
+            else:
+                # Adding the current line to the sequence list.
+                current_seq_list.append(line)
+
+        # Turning sequence list into one string.
+        current_seq = ''.join(current_seq_list)
+        try:
+            search = re.search(r'gene=[A-Za-z0-9]*', current_header)
+            gene_name = search.group().lower()
+        except AttributeError:
+            gene_name = current_header
+
+        nucleotide_dict[gene_name] = current_seq
+
+        # Setting the current line as the new current
+        # header.
+        current_header = line
+        # Resetting the current sequence list because
+        # the previous sequence has ended.
+        current_seq_list = []
+        return nucleotide_dict
+
+
+def seperate(nucleotide_dict):
+    surface_protein = {}
+    other = nucleotide_dict.copy()
+    other.pop('gene=nef')
+    surface_protein['gene=nef'] = nucleotide_dict['gene=nef']
+    return surface_protein, other
+
+
+if __name__ == '__main__':
+    x = get_sequences('fasta/virus/siv seq mRNA.fasta')
+    print(seperate(x)[0])
+
